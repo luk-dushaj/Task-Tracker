d0e5f69b34694414d92bec9b11ed3d507fbd490e
luk-dushaj <extents.lie_0y@icloud.com>
2024-10-15 15:45:52 -0400

    Added Task class

--- app/src/main/java/com/school/tasktracker/MainActivity.kt
+++ app/src/main/java/com/school/tasktracker/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
---
+++ app/src/main/java/com/school/tasktracker/Task.kt
@@ -0,0 +1,16 @@
+package com.school.tasktracker
+
+import java.util.Date
+import java.util.UUID
+
+// Implemented as data class because we are managing the objects in a array
+// All the standard methods like delete/create will be in the array
+// editing will mainly be accessing the properties from the objects via dot notation
+data class Task(
+    val id: UUID = UUID.randomUUID(),
+    var title: String,
+    var description: String,
+    var isPriority: Boolean,
+    var deadline: Date,
+    var isCompleted: Boolean = false
+)
